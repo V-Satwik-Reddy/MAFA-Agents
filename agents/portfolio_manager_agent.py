cfe0c2aca8150f885b6f65be4121d2642c20636a
V-Satwik-Reddy <satwikstudies05@gmail.com>
2026-01-07 22:22:09 +0530

    the base model of the agent

--- agents/portfolio_manager_agent.py
+++ agents/portfolio_manager_agent.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-from tools.profile_tools import get_user_holdings
+from tools.profile_tools import get_user_holdings, get_current_stock_price
 from tools.memory_tools import (
     retrieve_user_context,
     store_user_context,
@@ -15,4 +15,51 @@ from vectordbsupabase import SupabaseVectorDB
 load_dotenv()
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
-mode=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+model=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+vector_db = SupabaseVectorDB()
+
+tools=[
+    get_current_stock_price,
+    get_user_holdings,
+    search_user_memory,
+    store_user_note,
+]
+
+BASE_SYSTEM_PROMPT=""" 
+
+"""
+agent = create_agent(
+    tools=tools,
+    model=model,
+    system_message=BASE_SYSTEM_PROMPT,
+)
+
+def run_portfolio_manager_agent(user_message: str, user_id: int) -> str:
+    """Run the Portfolio Manager Agent with user message and context."""
+    print(f"\n{'='*60}")
+    print(f"USER {user_id}: {user_message}")
+    print(f"{'='*60}\n")
+    
+    response=agent.invoke(
+        {
+            "messages": [
+                {"role": "system", "content": BASE_SYSTEM_PROMPT},
+                {"role": "user", "content": user_message},
+            ],
+        },
+        config={"user_id": user_id},
+    )
+    final_message = response["messages"][-1]
+    def normalize(content):
+        if isinstance(content, list):
+            return "".join(
+                part["text"]
+                for part in content
+                if isinstance(part, dict) and part.get("type") == "text"
+            )
+        return content
+    print("AGENT:", final_message.content)
+    
+    agent_reply = normalize(final_message.content)
+    return agent_reply
